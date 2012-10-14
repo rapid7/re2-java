82f754ae787c07b77ed3c89b92e03bd1ebb0298f
Daniel Fiala <danfiala@ucw.cz>
2012-10-14 14:18:26 +0200

    Unfortunately java RE cannot process all possible generated RE in reasonable time.

--- test/com/logentries/re2_test/TestRandomExpr.java
+++ test/com/logentries/re2_test/TestRandomExpr.java
@@ -18,18 +18,26 @@ public class TestRandomExpr {
     private GenRegExpr genRegExpr = new GenRegExpr(mAlphabet, 3, 12);
     private GenString genString = new GenString(mAlphabet, 15);
 
-    private void compareOneRandExpr() {
+    private void compareOneRandExpr(final int index) {
         final String regExprStr = genRegExpr.next();
+        System.err.println("Runnig i = " + index + "\t" + regExprStr);
 
         Pattern pattern = Pattern.compile(regExprStr);
+        System.err.println("\t+Pattern.compile()");
         RE2 re2 = new RE2(regExprStr);
+        System.err.println("\t+new RE2()");
 
         for (int i = 0; i < 25; ++i) {
             final String str = genString.next();
+            System.err.println("\t" + str);
             final boolean matches = pattern.matcher(str).matches();
+            System.err.println("\t\tPattern.matches()");
             final boolean re2_matches = re2.fullMatch(str);
+            System.err.println("\t\tRE2.matches()");
             final boolean found = pattern.matcher(str).find();
+            System.err.println("\t\tPattern.find()");
             final boolean re2_found = re2.partialMatch(str);
+            System.err.println("\t\tRE2.partialMatch()");
             if (matches != re2_matches || found != re2_found) {
                 System.err.println("reg-expr:[" + regExprStr + "]; str:[" + str + "] " + matches + "\t" + re2_matches + "\t" + found + "\t" + re2_found);
             }
@@ -39,11 +47,50 @@ public class TestRandomExpr {
         re2.dispoze();
     }
 
-    @Test
     public void testRandExpr() {
         for (int i = 0; i < 200; ++i) {
-            System.err.println("Runnig i = " + i);
-            compareOneRandExpr();
+            compareOneRandExpr(i);
         }
+    }
+
+    @Test
+    public void testRandRE2() {
+        class Worker implements Runnable {
+            public void run() {
+                for (int i = 0; i < 2000; ++i) {
+                    runOneRandRE2(i);
+                }
+            }
+        }
+
+        Thread[] ths = new Thread[8];
+        for (int i = 0; i < ths.length; ++i) {
+            (ths[i] = new Thread(new Worker())).start();
+        }
+        for (int i = 0; i < ths.length; ++i) {
+            try {
+                ths[i].join();
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    public void runOneRandRE2(final int index) {
+        final String regExprStr = genRegExpr.next();
+        System.err.println("Runnig i = " + index + "\t" + regExprStr);
+
+        RE2 re2 = new RE2(regExprStr);
+        System.err.println("\t+new RE2()");
+
+        for (int i = 0; i < 25; ++i) {
+            final String str = genString.next();
+            System.err.println("\t" + str);
+            final boolean re2_matches = re2.fullMatch(str);
+            System.err.println("\t\tRE2.matches()");
+            final boolean re2_found = re2.partialMatch(str);
+            System.err.println("\t\tRE2.partialMatch()");
+            System.err.println("reg-expr:[" + regExprStr + "]; str:[" + str + "] " + re2_matches + "\t" + re2_found);
+        }
+        re2.dispoze();
     }
 }
