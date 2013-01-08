311abad4090f862aa3c15d02114a4ca4ee8753ab
stuart <stuart@logentries.com>
2013-01-08 11:31:56 +0000

    Using uname -m if uname -i returns unknown

--- bin/os-arch.sh
+++ bin/os-arch.sh
@@ -2,6 +2,11 @@
 
 UNAME=$(uname -i)
 
+# RHEL seems to use -m flag
+if [ "$UNAME" == 'unknown' ]; then
+    UNAME=$(uname -m)
+fi
+
 if [ "$UNAME" == 'x86_64' ]; then
     echo 'amd64'
 elif [ "$UNAME" == 'amd64' ]; then
